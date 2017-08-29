953df49e0a9e2bd74d185f8fe7b5574b10a4ba9e
Sander Saares <saares@axinom.com>
2017-08-29 12:50:35 +0300

    Initial commit

---
+++ cpix-validator/Default.aspx
@@ -0,0 +1,120 @@
+﻿<%@ page language="C#" autoeventwireup="true" codebehind="Default.aspx.cs" inherits="Axinom.CpixValidator.Default" %>
+
+<!DOCTYPE html>
+<html lang="en">
+<head runat="server">
+    <title>CPIX validator</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h1>CPIX document validator</h1>
+        <p>
+            This page validates the conformance of XML documents to the <a href="http://dashif.org/guidelines/">Content Protection Information Exchange (CPIX) 2.0</a> specification.
+        </p>
+
+        <asp:Panel runat="server" ID="resultsPanel" Visible="false">
+            <h2>Result</h2>
+
+            <asp:Panel runat="server" ID="negativeResultPanel" Visible="false">
+                <p>
+                    The CPIX document is <span class="badge badge-danger">INVALID</span>
+                </p>
+            </asp:Panel>
+
+            <asp:Panel runat="server" ID="positiveResultPanel" Visible="false">
+                <p>
+                    The CPIX document is <span class="badge badge-primary">potentially valid</span>
+                </p>
+                <p>
+                    Not every aspect of the CPIX specification is checked by this validator, so a definitive statement of validity is impossible.
+                </p>
+            </asp:Panel>
+
+            <h3>Details</h3>
+
+            <asp:Repeater runat="server" ID="errorList" ItemType="System.String">
+                <ItemTemplate>
+                    <div class="alert alert-danger"><%# Item %></div>
+                </ItemTemplate>
+            </asp:Repeater>
+
+            <asp:Repeater runat="server" ID="warningList" ItemType="System.String">
+                <ItemTemplate>
+                    <div class="alert alert-warning"><%# Item %></div>
+                </ItemTemplate>
+            </asp:Repeater>
+
+            <asp:Repeater runat="server" ID="messageList" ItemType="System.String">
+                <ItemTemplate>
+                    <div class="alert alert-info"><%# Item %></div>
+                </ItemTemplate>
+            </asp:Repeater>
+        </asp:Panel>
+
+        <h2>Input data</h2>
+        <asp:Repeater runat="server" ID="formErrorsList" ItemType="System.String">
+            <ItemTemplate>
+                <div class="alert alert-danger"><%# Item %></div>
+            </ItemTemplate>
+        </asp:Repeater>
+
+        <form id="form1" runat="server">
+            <div class="form-group row">
+                <label for="cpixFile" class="col-form-label col-sm-2">Select CPIX file</label>
+                <div class="col-sm-10">
+                    <input runat="server" type="file" accept=".xml" class="form-control" id="cpixFile" required />
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <span class="col-form-label col-sm-2">Recipient private key (optional)</span>
+
+                <div class="col-sm-10">
+                    <p>
+                        The validator will attempt to decrypt any encrypted content keys.
+                    </p>
+
+                    <p>
+                        <button type="button" class="btn btn-sm btn-info" data-toggle="collapse" href="#moreinfo">More info...</button>
+                    </p>
+
+                    <div id="moreinfo" class="collapse">
+                        <div class="card card-body">
+                            <p>This verifies that the cryptographic parameters are valid and the encrypted data can be decrypted successfully. Without decryption these elements cannot be thoroughly inspected.</p>
+                            <p>The CPIX document must contain a DeliveryData element identifying the subject of the certificate in the PFX file as a valid recipient.</p>
+                            <p>Do not upload any sensitive production data!</p>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="pfxFile" class="form-control-label">PFX file with certificate and private key</label>
+                        <input runat="server" type="file" accept=".pfx" class="form-control" id="pfxFile" />
+
+                        <label for="pfxPassword" class="form-control-label">PFX password</label>
+                        <input runat="server" type="password" class="form-control" id="pfxPassword" />
+                    </div>
+                </div>
+            </div>
+
+            <p>
+                <asp:Button runat="server" CssClass="btn btn-primary" Text="Validate" OnClick="Validate_Click" />
+            </p>
+        </form>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+    <script>
+        $(function () {
+            $('[data-toggle="popover"]').popover()
+        })
+    </script>
+</body>
+</html>
---
+++ cpix-validator/Default.aspx.cs
@@ -0,0 +1,230 @@
+﻿using Axinom.Cpix;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Axinom.CpixValidator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Validate_Click(object sender, EventArgs e)
+        {
+            // If not provided, the file will be empty but not null.
+            var cpixFile = Request.Files["cpixFile"];
+            var pfxFile = Request.Files["pfxFile"];
+
+            var pfxPassword = Request.Form["pfxPassword"];
+
+            // Form results (validation did not take place if any errors here).
+            var formErrors = new List<string>();
+
+            // Validation results.
+            var errors = new List<string>();
+            var warnings = new List<string>();
+            var messages = new List<string>();
+
+            // If form errors occur, we do not show validation results.
+            resultsPanel.Visible = false;
+
+            X509Certificate2 recipientCertificate = null;
+
+            try
+            {
+                if (pfxFile.ContentLength != 0)
+                {
+                    // If a PFX file is provided, a PFX password must be provided.
+                    if (string.IsNullOrWhiteSpace("pfxPassword"))
+                    {
+                        formErrors.Add("You must provide the PFX password if you provide a PFX file.");
+                        return;
+                    }
+
+                    byte[] pfxBytes;
+
+                    using (var reader = new BinaryReader(pfxFile.InputStream))
+                        pfxBytes = reader.ReadBytes(pfxFile.ContentLength);
+
+                    try
+                    {
+                        recipientCertificate = new X509Certificate2(pfxBytes, pfxPassword);
+                    }
+                    catch (Exception ex)
+                    {
+                        formErrors.Add("Unable to open the PFX file: " + ex.Message);
+                        return;
+                    }
+                }
+                else if (!string.IsNullOrWhiteSpace(pfxPassword))
+                {
+                    // You cannot provide only a PFX password with no PFX file - something went wrong.
+                    formErrors.Add("You provided a PFX password but no PFX file.");
+                    return;
+                }
+
+                // If we got this far, we got to the validation step and can show results.
+                resultsPanel.Visible = true;
+                positiveResultPanel.Visible = false;
+                negativeResultPanel.Visible = false;
+
+                CpixDocument cpix;
+
+                try
+                {
+                    // This can throw for a large number of reasons (most validation failures will come from here).
+                    // It is slightly annoying because we just get the first error but this is also minimum-effort solution.
+
+                    if (recipientCertificate != null)
+                    {
+                        cpix = CpixDocument.Load(cpixFile.InputStream, recipientCertificate);
+                    }
+                    else
+                    {
+                        cpix = CpixDocument.Load(cpixFile.InputStream);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    errors.Add(ex.Message);
+                    warnings.Add("Validation could not be completed. Fix the above error and try again to perform a full validation.");
+                    return;
+                }
+
+                // If it loaded, let's do some more checks for logical consistency.
+
+                if (cpix.ContentKeys.Any())
+                {
+                    if (cpix.ContentKeysAreReadable)
+                    {
+                        if (recipientCertificate == null)
+                        {
+                            warnings.Add("Content keys in the document are not encrypted. This CPIX document should not be used in production scenarios.");
+                        }
+                        else
+                        {
+                            messages.Add("Content keys in the document are encrypted. Decryption was successful.");
+                        }
+                    }
+                    else
+                    {
+                        if (recipientCertificate == null)
+                        {
+                            // Keys are not readable and we did not have the key. This is fine but we cannot validate
+                            // the key data like this, so emit a warning.
+                            warnings.Add("Encrypted content keys are present in the document but cannot be validated without the recipient private key. Provide the PFX file with the recipient's key pair to validate encrypted data.");
+                        }
+                        else
+                        {
+                            // We had a certificate but still could not read the content keys? Wrong certificate!
+                            errors.Add("The certificate in the PFX file was not defined as a valid recipient by the CPIX document.");
+                        }
+                    }
+                }
+
+                // And finally, just dump some useful data.
+                if (cpix.ContentKeys.Any())
+                {
+                    messages.Add($"The document contains {cpix.ContentKeys.Count} content keys: {string.Join(", ", cpix.ContentKeys.Select(k => k.Id.ToString()))}.");
+                }
+                else
+                {
+                    messages.Add("The document does not contain any content keys.");
+                }
+                
+                if (cpix.Recipients.Any())
+                {
+                    foreach (var recipient in cpix.Recipients)
+                        messages.Add($"The document defines {recipient.Certificate.Subject} as a valid recipient.");
+                }
+                else
+                {
+                    messages.Add("The document does not define any valid recipients.");
+                }
+
+                if (cpix.UsageRules.Any())
+                {
+                    messages.Add($"The document defines {cpix.UsageRules.Count} usage rules.");
+                }
+                else
+                {
+                    messages.Add("The document does not define any usage rules.");
+                }
+
+                if (cpix.ContentKeys.SignedBy != null)
+                {
+                    foreach (var signer in cpix.ContentKeys.SignedBy)
+                        messages.Add($"The set of content keys is digitally signed by {signer.Subject}");
+                }
+                else if (cpix.ContentKeys.Any())
+                {
+                    messages.Add("The set of content keys is not digitally signed.");
+                }
+
+                if (cpix.UsageRules.SignedBy != null)
+                {
+                    foreach (var signer in cpix.UsageRules.SignedBy)
+                        messages.Add($"The set of usage rules is digitally signed by {signer.Subject}");
+                }
+                else if (cpix.UsageRules.Any())
+                {
+                    messages.Add("The set of usage rules is not digitally signed.");
+                }
+
+                if (cpix.Recipients.SignedBy != null)
+                {
+                    foreach (var signer in cpix.Recipients.SignedBy)
+                        messages.Add($"The set of valid recipients is digitally signed by {signer.Subject}");
+                }
+                else if (cpix.Recipients.Any())
+                {
+                    messages.Add("The set of valid recipients is not digitally signed.");
+                }
+
+                if (cpix.SignedBy != null)
+                {
+                    messages.Add($"The document as a whole is signed by {cpix.SignedBy.Subject}");
+                }
+                else
+                {
+                    messages.Add("The document as a whole is not digitally signed.");
+                }
+            }
+            catch (Exception ex)
+            {
+                formErrors.Add("Unexpected processing error. Please report this on GitHub! " + ex.Message);
+            }
+            finally
+            {
+                if (recipientCertificate != null)
+                    recipientCertificate.Dispose();
+
+                formErrorsList.Visible = formErrors.Any();
+
+                if (formErrors.Any())
+                {
+                    formErrorsList.DataSource = formErrors;
+                    formErrorsList.DataBind();
+                }
+
+                errorList.DataSource = errors;
+                errorList.DataBind();
+
+                warningList.DataSource = warnings;
+                warningList.DataBind();
+
+                messageList.DataSource = messages;
+                messageList.DataBind();
+
+                if (errors.Any())
+                {
+                    negativeResultPanel.Visible = true;
+                }
+                else
+                {
+                    positiveResultPanel.Visible = true;
+                }
+            }
+        }
+    }
+}
